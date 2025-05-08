7c55a616885fb0391ac0e1c3b4cd609b72eab344
Shao Zhi <soongshaozhi@gmail.com>
2025-05-08 20:09:17 +0800

    hid supabase key in env file

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'https://kqhkprbujcmfgfhlhxud.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxaGtwcmJ1amNtZmdmaGxoeHVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2OTE4NjYsImV4cCI6MjA2MjI2Nzg2Nn0.7cOjyRfag-U2V9EefU-lk-LJzZB7cYxOTuktxxMvsdI';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
